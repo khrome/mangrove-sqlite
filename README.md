c9ce149836d2919aa240642b152f6a38c1bae3c6
Abbey Sparrow <abbey@khrome.net>
2021-11-17 21:01:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+mangrove-sqlite
+===============
+A [Mangrove](https://www.npmjs.com/package/mangrove) adapter to read and write out of [SQLite](https://www.sqlite.org/).
---
+++ driver.js
@@ -0,0 +1,82 @@
+const SQLite3 = require('better-sqlite3');
+module.exports = {
+  Adapter : require('mangrove-sql-adapter').extend({
+      cleanup : function(){
+          try{
+
+          }catch(ex){}
+      },
+      client : function(usePooled){
+          return SQLite3(this.options.database, {});
+      },
+      connect: function(name, options, handler, cb){
+
+      },
+      loaderResultsHandler: function(err, res, itemHandler, cb){
+
+      },
+      existsResultsHandler: function(err, res, itemHandler, cb){
+
+      },
+      createResultsHandler: function(err, res, itemHandler, cb){
+
+      },
+      saveResultsHandler: function(err, res, itemHandler, cb){
+
+      },
+      sqlTypeFromJavascriptType : function(name, value, pk){
+          var type = typeof value;
+          if(type === 'object' && Array.isArray(value)) type = 'array';
+          if(type === 'object' && value instanceof Date) type = 'datetime';
+          if(type === 'number' && !isNaN(value)){
+              // check if it is integer
+              if( Number.isInteger(value) ) type = 'integer';
+              else type = 'float';
+          }
+          var typeCreate;
+          switch(type){
+              case 'string' : typeCreate = 'VARCHAR (255)'; break;
+              case 'integer' : typeCreate = 'INTEGER'; break;
+              case 'bigint' : typeCreate = 'BIGINT'; break;
+              case 'object' : typeCreate = 'JSON'; break;
+              case 'float' : typeCreate = 'FLOAT (8)'; break;
+              //todo: handle arrays + arrays of objects as FKs
+          }
+          if(!typeCreate) throw new Error('Unrecognized Type: '+type);
+          if(name === pk ) return name + ' ' + typeCreate+ ' PRIMARY KEY';
+          return name + ' ' + typeCreate+ ' NOT NULL';
+      },
+      createSQL: function(tableName, object, pk){
+          var fields = Object.keys(object);
+          var creates = [];
+          fields.forEach((field)=>{
+              creates.push(this.sqlTypeFromJavascriptType(field, object[field], pk));
+          });
+          var sql = 'CREATE TABLE '+tableName+'('+creates.join(', ')+')';
+          return sql;
+      },
+      loaderSQL: function(tableName){
+          return "SELECT * from "+tableName;
+      },
+      existsSQL : function(tableName){
+
+      },
+      saveSQL : function(table, fields, pk, object, valuesFn){
+          var values = [];
+          fields.forEach((field)=>{
+              values.push(object[field])
+          });
+          //everything needs to be rendered as symbols, so pg escaping can work
+          var symbols = values.map((item, index)=> '$'+(index+1) );
+          var commas = ', ';
+          var query = 'INSERT INTO '+table+'('+fields.join(commas)+') '+
+                      'VALUES ('+symbols.join(commas)+') '+
+                      'ON CONFLICT('+pk+') '+
+                      'DO UPDATE SET '+ fields.map(
+                          (name, index)=> name+' = '+symbols[index]
+                      ).join(commas);
+          if(valuesFn) valuesFn(values);
+          return query;
+      }
+  })
+}
